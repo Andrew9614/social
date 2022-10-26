3982bf5399d85229463e4c6ba1e98dea3be30cec
Andrew9614 <54529269+Andrew9614@users.noreply.github.com>
2022-10-26 15:22:05 +0300

    lesson 18

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,11 @@
+import styles from './Dialogs.module.css'
+
+type DialogsProps = any;
+
+export const Dialogs: React.FC<DialogsProps> = (props) => {
+	return (
+		<div>
+			Dialogs
+		</div>
+	)
+}
