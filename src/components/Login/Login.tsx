1af903fe871d7b11edea2d0f909dace3aca3bf1c
Andrew9614 <54529269+Andrew9614@users.noreply.github.com>
2023-03-25 11:30:45 +0300

    login error message mobile fix

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -28,83 +28,89 @@ export const Login = () => {
   return redirect ? (
     <Navigate to={redirectPath} />
   ) : (
-    <Formik
-      initialValues={{
-        email: '',
-        // password: '4ARNrECxbXj6nKS',
-        password: '',
-        rememberMe: false,
-        captcha: '',
-      }}
-      validate={(values) => {
-        const errors: { [key: string]: string } = {};
-        if (!values.email) {
-          errors.email = 'Email required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        if (!values.password) {
-          errors.password = 'Password required';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        //console.log(JSON.stringify(values, null, 2));
-        dispatch(loginUser(values)).then((res) => res && setRedirect(true));
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form className={styles.formWrapper}>
-          <div className={styles.formContainer}>
-            <div className={styles.inputsWrapper}>
-              <div className={styles.inputsContainer}>
-                <div className={styles.inputContainer}>
-                  <Field type="email" name="email" placeholder="Email:" />
-                  <ErrorMessage
-                    className={styles.errorMessage}
-                    name="email"
-                    component="div"
-                  />
+    <div>
+      <Formik
+        initialValues={{
+          email: '',
+          // password: '4ARNrECxbXj6nKS',
+          password: '',
+          rememberMe: false,
+          captcha: '',
+        }}
+        validate={(values) => {
+          const errors: { [key: string]: string } = {};
+          if (!values.email) {
+            errors.email = 'Email required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          if (!values.password) {
+            errors.password = 'Password required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          //console.log(JSON.stringify(values, null, 2));
+          dispatch(loginUser(values)).then((res) => res && setRedirect(true));
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className={styles.formWrapper}>
+            <div className={styles.formContainer}>
+              <div className={styles.inputsWrapper}>
+                <div className={styles.inputsContainer}>
+                  <div className={styles.inputContainer}>
+                    <Field type="email" name="email" placeholder="Email:" />
+                    <ErrorMessage
+                      className={styles.errorMessage}
+                      name="email"
+                      component="div"
+                    />
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <Field
+                      type="password"
+                      name="password"
+                      placeholder="Password:"
+                    />
+                    <ErrorMessage
+                      className={styles.errorMessage}
+                      name="password"
+                      component="div"
+                    />
+                  </div>
                 </div>
-                <div className={styles.inputContainer}>
-                  <Field
-                    type="password"
-                    name="password"
-                    placeholder="Password:"
-                  />
-                  <ErrorMessage
-                    className={styles.errorMessage}
-                    name="password"
-                    component="div"
-                  />
+                <div className={styles.submitContainer}>
+                  <button type="submit" disabled={isSubmitting}>
+                    Log In
+                  </button>
+                  {responseMessage && (
+                    <div className={styles.errorMessage}>{responseMessage}</div>
+                  )}
                 </div>
               </div>
-              <div className={styles.submitContainer}>
-                <button type="submit" disabled={isSubmitting}>
-                  Log In
-                </button>
-                {responseMessage && (
-                  <div className={styles.errorMessage}>{responseMessage}</div>
-                )}
-              </div>
-            </div>
 
-            <div className={styles.checkboxContainer}>
-              <Field type="checkbox" name="remember" />
-              <label htmlFor="rememberMe">Remember me</label>
-            </div>
-            {!captcha || (
-              <div className={styles.captchaContainer}>
-                <img src={captcha} alt="captcha" />
-                <Field name="captcha" placeholder="Write text from image:" />
+              <div className={styles.checkboxContainer}>
+                <Field type="checkbox" name="remember" />
+                <label htmlFor="rememberMe">Remember me</label>
               </div>
-            )}
-          </div>
-        </Form>
-      )}
-    </Formik>
+              {!captcha || (
+                <div className={styles.captchaContainer}>
+                  <img src={captcha} alt="captcha" />
+                  <Field name="captcha" placeholder="Write text from image:" />
+                </div>
+              )}
+            </div>
+          </Form>
+        )}
+      </Formik>
+			<div className={styles.infoText}>
+				<p>Test login: free@samuraijs.com <br />
+				Test password: free</p>
+			</div>
+    </div>
   );
 };
